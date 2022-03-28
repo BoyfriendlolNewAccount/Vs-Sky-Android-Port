c277efeb0558d3da7ad55bd7c6451337ccc8dd6e
Arandomguylool <102329658+Arandomguylool@users.noreply.github.com>
2022-03-28 20:01:29 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -11,7 +11,6 @@ import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
-import io.newgrounds.NG;
 import lime.app.Application;
 
 #if windows
@@ -119,7 +118,7 @@ class MainMenuState extends MusicBeatState
 		changeItem();
 		
 		#if android
-		addVirtualPad(UP_DOWN);
+		addVirtualPad(UP_DOWN, A_B);
 		#end
 
 		super.create();
