c2f2d6e0870dbec2084bef764f4acdfc7cece8cc
Arandomguylool <102329658+Arandomguylool@users.noreply.github.com>
2022-03-28 20:38:00 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -33,6 +33,8 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		
+ 	        SUtil.gameCrashCheck();
 
 		if (stage != null)
 		{
@@ -71,6 +73,8 @@ class Main extends Sprite
 		#if !debug
 		initialState = TitleState;
 		#end
+			
+		SUtil.doTheCheck();
 
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 
